253ca75389f6d5e13e131ad28b2b61fd5ede08a1
Renzo Osorio <renzo.osorio1909@gmail.com>
2023-02-05 01:26:00 -0500

    sección de useContext

---
+++ 05-hook-app/src/09-useContext/context/UserProvider.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { UserContext } from "./UserContext";
+
+// const user = {
+//   id: 123,
+//   name: "Pepito",
+//   email: "pepito@gmail.com",
+// };
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
+  return (
+    // <UserContext.Provider value={{ hola: "Mundo", user }}>
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
