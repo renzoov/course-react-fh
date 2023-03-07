0464c90c368d69b9e15dc31f28356cc41c17d54b
Renzo Osorio <renzo.osorio1909@gmail.com>
2023-03-06 22:09:23 -0500

    agregando app toolkit-redux

--- 09-toolkit-redux/src/store/store.js
+++ 09-toolkit-redux/src/store/store.js
@@ -1,5 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { todosApi } from "./apis";
+import { counterSlice } from "./slices/counter";
+import { pokemonSlice } from "./slices/pokemon";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterSlice.reducer,
+    pokemmons: pokemonSlice.reducer,
+
+    [todosApi.reducerPath]: todosApi.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todosApi.middleware),
 });
